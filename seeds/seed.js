0d84e1ceffc47562675328eb4ffd1b1d509cbf86
SV <72838881+santis1001@users.noreply.github.com>
2023-06-19 20:30:56 -0600

    Models index relations - database seeds: - seeds.js - models JSON

---
+++ seeds/seed.js
@@ -0,0 +1,21 @@
+const sequelize = require('../config/connection');
+const { User, Objective, Transaction } = require('../Models');
+
+const userData = require('./userData.json');
+const objectiveData = require('./ObjectiveData.json');
+const transactionData = require('./TransactionData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const objective = await Objective.bulkCreate(objectiveData);
+
+    const transaction = await Objective.bulkCreate(transactionData);
+
+    process.exit(0);
+};

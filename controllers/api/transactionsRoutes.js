ecd3a1d81a313062204f2ea6181616594d7c1dfc
Jessica Sanchez <jessamesan@hotmail.com>
2023-06-19 21:58:09 -0600

    Route part 2

--- controllers/api/transactionsRoutes.js
+++ controllers/api/transactionsRoutes.js
@@ -1,2 +1,4 @@
 const router = require('express').Router();
 const { Transaction } = require('../../Models');
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,4 @@
 const router = require('express').Router();
 const { User } = require('../../Models');
 
+module.exports = router;

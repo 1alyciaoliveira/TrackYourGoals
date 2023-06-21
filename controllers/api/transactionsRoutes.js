d237f36496dcb6ed229c8d42c23fb94f9e9e3d8c
SV <72838881+santis1001@users.noreply.github.com>
2023-06-20 20:40:34 -0600

    Working (theoretical data) - Homeroutes - transactionRoute

--- controllers/api/transactionsRoutes.js
+++ controllers/api/transactionsRoutes.js
@@ -3,21 +3,39 @@ const { Transaction } = require('../../Models');
 
 router.post('/', async (req, res) => {
     try {
-        res.status(200).json('request post');
+        const transactionData = await Transaction.create(
+            {
+                ...req.body, user_id: req.session.user_id
+            }
+        );
+        !transactionData[0] ? res.status(404).json({ message: 'Objective not found' }) :
+            res.status(200).json(transactionData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 router.put('/:id', async (req, res) => {
     try {
-        res.status(200).json('request put');
+        const transactionData = await Transaction.update(
+
+            req.body, { where: { id: req.params.id, user_id: req.session.user_id } }
+
+        );
+        !transactionData[0] ? res.status(404).json({ message: 'Objective not found' }) :
+            res.status(200).json(transactionData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 router.delete('/:id', async (req, res) => {
     try {
-        res.status(200).json('request delete');
+        const transactionData = await Transaction.destroy(
+            {
+                where: { id: req.params.id, user_id: req.session.user_id }
+            }
+        );
+        !transactionData[0] ? res.status(404).json({ message: 'Objective not found' }) :
+            res.status(200).json(transactionData);
     } catch (err) {
         res.status(500).json(err);
     }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,17 @@
 const router = require('express').Router();
-const { User , Transaction, Objective } = require('../Models');
+const { User, Transaction, Objective } = require('../Models');
+
+
+router.get('/user', async (req, res) => {
+    try {
+        const data = await User.findAll({
+            include: [{ model: Objective, include: { model: Transaction } }]
+        });
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;

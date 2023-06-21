283ecf71da54c9da3af63b0aa1b860f0b1ff1fab
Jessica Sanchez <jessamesan@hotmail.com>
2023-06-21 14:13:00 -0600

    Auth proposal , More seeds to test REQUESTS on Insomnia

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -68,6 +68,27 @@ router.get('/', async (req, res) => {
     router.get('/profile', (req, res) => {
         res.render('profile');
     });
-  
+
+    //== prpouesta uso Auth 
+    /*router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Objective }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+     */
 
 module.exports = router;

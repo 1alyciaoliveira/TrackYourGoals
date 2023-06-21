add0bf30f2dc020e3afebb10a02cfa27f9890ba1
Aly Oliveira <1alycia.oliveira@gmail.com>
2023-06-20 21:18:08 -0600

    updating objectiveRoutes to avoid conflict

--- controllers/api/objectiveRoutes.js
+++ controllers/api/objectiveRoutes.js
@@ -1,16 +1,6 @@
 const router = require('express').Router();
 const { Objective } = require('../../Models');
 
-router.get('/', async (req, res) => {
-    try {
-        const objectiveData = await Objective.findByPk(req.params.id);
-        const objective = objectiveData.get({ plain: true });
-        res.render('objective', objective);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 router.post('/', async (req, res) => {
     try {
         res.status(200).json('request post');

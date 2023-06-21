d235302f637998a9d25dc6fa8d7734a45612c617
Aly Oliveira <1alycia.oliveira@gmail.com>
2023-06-20 19:48:50 -0600

    updating handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
 const { User , Transaction, Objective } = require('../Models');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const objectiveData = await Objective.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const objectives = objectiveData.map((objective) => objective.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('singup', { //edit 'homepage' to the page you wanna render
+        objectives, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 module.exports = router;

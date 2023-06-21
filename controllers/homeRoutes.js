dfc41ca3383bd4ad8a60b385129f556b66734df9
SV <72838881+santis1001@users.noreply.github.com>
2023-06-20 20:40:34 -0600

    Working (theoretical data) - Homeroutes - transactionRoute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
-const { User , Transaction, Objective } = require('../Models');
+const { User, Transaction, Objective } = require('../Models');
+
+
+router.get('/user', async (req, res) => {
+    try {
+        const data = await User.findAll({
+            include: [{ model: Objective, include: { model: Transaction } }]
+        });
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 <<<<<<< HEAD
 router.get('/', async (req, res) => {
